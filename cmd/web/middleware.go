845f1fc98ce92e90bb5d8be27ba2a1880f3c865c
AppleThomas <thomasnguyen3212@gmail.com>
2023-08-15 18:15:05 -0400

    Added middleware, includes secure http heading, log requests, and panic recovery

---
+++ cmd/web/middleware.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func secureHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Content-Security-Policy",
+			"default-src 'self'; style-src 'self' fonts.googleapis.com; font-src fonts.gstatic.com")
+
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "0")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.infoLog.Printf("%s - %s %s %s", r.RemoteAddr, r.Proto, r.Method, r.URL.RequestURI())
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// create a deferred fucntion which will always be run in the event of a panic
+		defer func() {
+			// recover func checks if there's a panic or not
+			if err := recover(); err != nil {
+				// set a "Connection: close" header on the response.
+				w.Header().Set("Connection", "close")
+
+				// return 500 Internal Server Error
+				app.serverError(w, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
